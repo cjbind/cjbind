0c09b0a6678adaa8be0dcfaeac31fd12f23894e1
Zxilly <zxilly@outlook.com>
2025-01-31 02:38:35 +0800

    feat: support type codegen

--- testdata/test.h
+++ testdata/test.h
@@ -2,3 +2,9 @@ struct Structure {
     int foo;
     float bar;
 };
+
+typedef void (*OperationCallback)(int);
+
+void register_callback(void (*callback)(int));
+
+void perform_operation(int value, OperationCallback callback);
