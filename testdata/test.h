e7dff220ed8907e25c698a0990e5d4501f0a668d
Zxilly <zxilly@outlook.com>
2025-01-31 04:36:58 +0800

    feat: implement var codegen

--- testdata/test.h
+++ testdata/test.h
@@ -8,3 +8,6 @@ typedef void (*OperationCallback)(int);
 void register_callback(void (*callback)(int));
 
 void perform_operation(int value, OperationCallback callback);
+
+static const char* GREETING = "Hello";  // 只读字符串
+static char message[] = "Initial中文字符";      // 可修改字符数组（但每个副本独立）
