43130ded1304d908d4be0553b22cd44a35619e51
Zxilly <zxilly@outlook.com>
2025-02-02 03:57:11 +0800

    feat: finish codegen

--- testdata/test.h
+++ testdata/test.h
@@ -1,13 +1,11 @@
-struct Structure {
-    int foo;
-    float bar;
+enum E1 {
+    A,
+    B,
+    C
 };
 
-typedef void (*OperationCallback)(int);
-
-void register_callback(void (*callback)(int));
-
-void perform_operation(int value, OperationCallback callback);
-
-static const char* GREETING = "Hello";  // 只读字符串
-static char message[] = "Initial中文字符";      // 可修改字符数组（但每个副本独立）
+enum {
+    UA,
+    UB,
+    UC
+};
